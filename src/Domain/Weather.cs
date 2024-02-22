acfce62091fae65593b3286d5a9c6a9852fc604d
Antanas Ramanauskas <antanasramana@gmail.com>
2024-02-22 13:44:22 +0000

    Added Domain

---
+++ src/Domain/Weather.cs
@@ -0,0 +1,13 @@
+﻿namespace Domain;
+
+public record Weather
+{
+    public int Id { get; set; }
+    // Added timestamp of weather so that persistence of data makes sense
+    public DateTimeOffset TimeStamp { get; set; }
+    public int Temperature { get; set; }
+    public int Precipitation { get; set; }
+    public double WindSpeed { get; set; }
+    public required string Summary { get; set; }
+    public required string City { get; set; }
+}
